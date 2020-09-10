94758099499902cfe8eb661378498cbc5bef4b28
Marcin Polak <mapoart@gmail.com>
2020-09-10 16:31:51 +0200

    Changed folder

--- install/installRakudoStar.sh
+++ install/installRakudoStar.sh
@@ -1,7 +1,6 @@
 mkdir ~/rakudo && cd $_
 
 is_user_root () { [ ${EUID:-$(id -u)} -eq 0 ]; }
-
 if is_user_root; then apt install -y curl; else sudo apt install -y curl; fi
 
 curl -LJO https://rakudo.org/latest/star/src
@@ -14,10 +13,10 @@ make
 
 # If you wish, you can run the tests
 # Depending on your machine, they could take over half an hour to run
-make rakudo-test
-make rakudo-spectest
+# make rakudo-test
+# make rakudo-spectest
 
 make install
 
-echo "export PATH=$(pwd)/install/bin/:$(pwd)/install/share/perl6/site/bin:\$PATH" >> ~/.bashrc
+echo "export PATH=\"\$HOME/rakudo/install/bin/:\$HOME/rakudo/install/share/perl6/site/bin:\$PATH"\" >> ~/.bashrc
 source ~/.bashrc
